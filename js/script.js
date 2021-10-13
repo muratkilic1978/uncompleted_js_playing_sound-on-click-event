980a224a8a1afc2027338b9e12d76dceae9df7d9
Murat Kilic <muratkilic1978@gmail.com>
2021-10-13 07:26:51 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+    /* VARIABLES: */
+    /* Step 1.1 - Use document.getElementById to fecth the id named "monkeyAudio" and store the value in a variable (const) named "monkey" followed by a semicolon */
+    
+    
+    /* Step 1.2 - Use document.getElementById to fecth the id named "hornAudio" and store the value in a variable (const) named "horn" followed by a semicolon */
+
+    
+
+    /* FUNCTIONS: */
+    /* Step 3 - Create a function named playMonkeyAudio() { } with open and end curly-brackets */
+    /* Step 3.1 - Inside the curly-brackets in the function - do following. Write monkey.play() followed by a semicolon */
+
+
+
+    /* Step 3.2 - Create another function named pauseMonkeyAudio() { } with open and end curly-brackets */
+    /* Step 3.3 - Inside the curly-brackets in the function - do following. Write monkey.pause() followed by a semicolon  */
+
+
+
+    /* Step 3.4 - Create a third function named playHornAudio() { } with open and end curly-brackets */
+    /* Step 3.5 - Inside the curly-brackets in the function - do following. Write horn.play() followed by a semicolon */
+
+
+
+    /* Step 3.6 - Create a fourth function named pauseHornAudio() { } with open and end curly-brackets */
+    /* Step 3.7 - Inside the curly-brackets in the function - do following. Write horn.pause() followed by a semicolon  */
+
+
+
+   
